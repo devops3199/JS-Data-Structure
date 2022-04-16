1af0176e7e5f0aade634f255ebeb3f6e185b4e0d
Ray Jeong <devops3199@gmail.com>
2022-04-16 23:27:33 +0900

    chore: comment

--- Data Structure/Graphs/Graph.js
+++ Data Structure/Graphs/Graph.js
@@ -1,30 +1,30 @@
 class Graph {
-    constructor() {
-        this.adjacencyList = {};
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
     }
 
-    addVertex(vertex) {
-        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-    }
-
-    addEdge(v1, v2) {
-        this.adjacencyList[v1].push(v2);
-        this.adjacencyList[v2].push(v1);
-    }
-
-    removeEdge(v1, v2) {
-        this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
-        this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
-    }
-
-    removeVertex(vertex) {
-        while(this.adjacencyList[vertex].length) {
-            const adjacentVertex = this.adjacencyList[vertex].pop();
-            this.removeEdge(vertex, adjacentVertex);
-        }
-
-        delete this.adjacencyList[vertex];
-    }
+    delete this.adjacencyList[vertex];
+  }
 }
 
 const graph = new Graph();
@@ -32,4 +32,5 @@ graph.addVertex("Seoul");
 graph.addVertex("Brisbane");
 graph.addVertex("New York");
 graph.addEdge("Seoul", "Brisbane");
+graph.addEdge("Seoul", "New York");
 console.log(graph);
--- HangHae99 Questions/Maze.js
+++ HangHae99 Questions/Maze.js
@@ -1,37 +1,43 @@
-let maze = [[1,0,1,0,1,0], [1,1,1,1,1,1], [0,0,0,0,0,1], [1,1,1,1,1,1], [1,1,1,1,1,1]];
-let dx = [0,0,-1,1];
-let dy = [-1,1,0,0];
+let maze = [
+  [1, 0, 1, 0, 1, 0],
+  [1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1],
+];
+
+let dx = [0, 0, -1, 1];
+let dy = [-1, 1, 0, 0];
 let nx = 0;
 let ny = 0;
 
 function bfs(x, y) {
-    let queue = [[x,y]];
-    while(queue.length > 0) {
-        let temp = queue.pop(0);
-        x = temp[0];
-        y = temp[1];
+  let queue = [[x, y]];
+  while (queue.length > 0) {
+    let temp = queue.pop(0);
+    x = temp[0];
+    y = temp[1];
 
-        for(let i=0; i<4; i++) {
-            nx = x + dx[i];
-            ny = y + dy[i];
+    for (let i = 0; i < 4; i++) {
+      nx = x + dx[i];
+      ny = y + dy[i];
 
-            if(nx < 0 || ny < 0 || nx >= maze.length || ny >= maze[0].length) {
-                continue;
-            }
+      if (nx < 0 || ny < 0 || nx >= maze.length || ny >= maze[0].length) {
+        continue;
+      }
 
-            if(maze[nx][ny] === 0) {
-                continue;
-            } 
+      if (maze[nx][ny] === 0) {
+        continue;
+      }
 
-            if(maze[nx][ny] === 1) {
-                maze[nx][ny] = maze[x][y] + 1;
-                queue.push([nx, ny]);
-            }
-        }
+      if (maze[nx][ny] === 1) {
+        maze[nx][ny] = maze[x][y] + 1;
+        queue.push([nx, ny]);
+      }
     }
-    return maze[maze.length-1][maze[0].length-1];
+  }
+  return maze[maze.length - 1][maze[0].length - 1];
 }
 
-console.log(bfs(0,0));
+console.log(bfs(0, 0));
 console.log(maze);
-
